4d1a20903b2ce4ec15598fb6a012720bb05faf0b
Dmitriy <dumitrashikeu@gmail.com>
2022-07-27 21:49:19 +0300

    create image model / migration

---
+++ database/migrations/2022_07_27_183813_create_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('imageable_id');
+            $table->string('imageable_type');
+            $table->string('filename');
+            $table->string('path');
+            $table->boolean('from_url')->nullable()->default(0);
+            $table->string('disk')->nullable()->default('public');
+            $table->string('alt')->nullable();
+            $table->string('title')->nullable();
+            $table->string('scope')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};
