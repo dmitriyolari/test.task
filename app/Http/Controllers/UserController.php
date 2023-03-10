cd7cb4be312a292dd95b0519a1a06f6562e22529
dumitruolari <Dumitru.Olari@draytus.com>
2023-03-10 10:50:42 +0200

    rename UserCreateRequest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\User\CreateUserRequest;
+use App\Http\Requests\User\UserCreateRequest;
 use App\Http\Resources\User\UserResource;
 use App\Services\UserCreateService;
 
 class UserController extends Controller
 {
-    public function register(CreateUserRequest $request, UserCreateService $service): UserResource
+    public function register(UserCreateRequest $request, UserCreateService $service): UserResource
     {
         $validatedUser = $request->validated();
         $user = $service->create($validatedUser);
