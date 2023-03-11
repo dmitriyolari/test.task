22538014da2ebfda8c4a7bcef616134c540ab654
dumitruolari <Dumitru.Olari@draytus.com>
2023-03-11 21:53:39 +0200

    - write registration test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,12 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -17,5 +20,13 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testSuccessfulUserRegistration()
+    {
+        $fixture = $this->getFixtureJson('user-register.json');
+        $response = $this->post('api/v1/user/register', $fixture, ['Accept' => 'application/json']);
+
+        $response->assertStatus(201);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -7,4 +9,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function getFixtureJson(string $path)
+    {
+        $filePath = $_SERVER['TESTS_PATH'] . '/Fixtures/' . $path;
+        return json_decode(file_get_contents($filePath), true);
+    }
 }
